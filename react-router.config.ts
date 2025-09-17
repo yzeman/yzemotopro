9a2594638bf2e786c71a5b872b6af7d8fbb4ff26
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-17 17:04:51 +0100

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
 	appDirectory: './src/app',
-	ssr: true,
+	ssr: false,
 	prerender: [], // ← CHANGED FROM ['/*?'] TO []
 } satisfies Config;
