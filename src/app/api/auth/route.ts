e65604a3e3e9fbfa1837f39383cfca1f82742c54
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-17 15:34:51 +0100

    Create route.ts

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,9 @@
+import { type ActionFunctionArgs, type LoaderFunctionArgs } from 'react-router';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return new Response('Auth API', { status: 200 });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  return new Response('Method not allowed', { status: 405 });
+}
