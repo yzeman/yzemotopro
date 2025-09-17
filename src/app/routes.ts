594a8b232100c85c30e4addb221532c8c9a16154
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-17 16:03:57 +0100

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,14 @@
+import {
+  type RouteConfigEntry,
+  index, // <-- ADD THIS
+  route,
+} from '@react-router/dev/routes';
+
 export default [
   index('./page.jsx'),
   route('admin', './admin/page.jsx'),
   route('vehicles/:id', './vehicles/[id]/page.jsx'),
   route('api/auth/*', './api/auth/route.ts'),
-  route('api/vehicles/*', './api/vehicles/route.js'), // Fixed extension
+  route('api/vehicles/*', './api/vehicles/route.js'),
   route('*', './__create/not-found.tsx'),
 ];
