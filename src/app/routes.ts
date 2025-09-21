03013eaf92fb9fa5b83847c106ca76811e0be676
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-21 20:41:18 +0100

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,22 +3,22 @@ import { type RouteConfig } from '@react-router/dev/routes';
 export default [
   {
     path: '/',
-    component: './routes/_index.tsx',
+    file: './routes/_index.tsx',
   },
   {
     path: 'admin',
-    component: './admin/page.jsx',
+    file: './admin/page.jsx',
   },
   {
     path: 'vehicles/:id',
-    component: './vehicles/[id]/page.jsx',
+    file: './vehicles/[id]/page.jsx',
   },
   {
     path: 'api/auth/*',
-    component: './api/auth/route.ts',
+    file: './api/auth/route.ts',
   },
   {
     path: '*',
-    component: './__create/not-found.tsx',
+    file: './__create/not-found.tsx',
   }
 ] satisfies RouteConfig;
