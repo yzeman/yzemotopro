d879b189069ae56278a243ec071e24d3e47c5a32
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-22 07:20:12 +0100

    Update ssr.js

--- netlify/functions/ssr.js
+++ netlify/functions/ssr.js
@@ -1,11 +1,18 @@
+// Option 1: Try named import
 import { createRequestHandler } from "@react-router/node";
+
+// Option 2: Try default import if named doesn't work
+// import createRequestHandler from "@react-router/node";
+
+// Option 3: Try the entire namespace
+// import * as reactRouterNode from "@react-router/node";
+// const { createRequestHandler } = reactRouterNode;
 
 export const handler = createRequestHandler({
   build: async () => {
-    // Use dynamic import with the correct path
     const serverBuild = await import(
       process.cwd() + "/build/server/index.js"
     );
     return serverBuild;
   },
-});
+});qqqqq
